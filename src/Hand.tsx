0dc8fcbdfa23ac666f7671749232d651ba4a152c
high1 <high1@users.noreply.github.com>
2022-09-22 01:14:29 +0200

    refactor: optimized props destructuring (#9)
    - it's not needed to destructure transform inside HandProps, it's enough
    to pass it with the ...rest props

--- src/Hand.tsx
+++ src/Hand.tsx
@@ -13,14 +13,12 @@ export const Hand = ({
   length = 0,
   limit = 94,
   stationary,
-  transform,
   ...rest
 }: HandProps) => (
+  // https://github.com/preactjs/signals/issues/106
+  // @ts-expect-error Type 'ReadonlySignal<string>' is not assignable to type 'string'.ts(2322)
   <line
     className={`stroke-cap-round ${className}`}
-    // https://github.com/preactjs/signals/issues/106
-    // @ts-expect-error Type 'ReadonlySignal<string>' is not assignable to type 'string'.ts(2322)
-    transform={transform}
     y1={stationary ? length - limit : undefined}
     y2={-(stationary ? limit : length)}
     {...rest}
