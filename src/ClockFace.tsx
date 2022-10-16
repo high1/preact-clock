359b5e94e14c8f48c76876dd422760a8a5de4b55
high1 <high1@mailfence.com>
2022-10-16 17:43:02 +0200

    fix: removed will change optimization

--- src/ClockFace.tsx
+++ src/ClockFace.tsx
@@ -50,7 +50,7 @@ export const ClockFace = () => {
         <g className="translate-100px">
           <ClockHand
             transform={miliseconds}
-            class="stroke-neutral-200 @dark:stroke-neutral-600 stroke-width-5 will-change-transform"
+            class="stroke-neutral-200 @dark:stroke-neutral-600 stroke-width-5"
             length={83}
           />
           <ClockHand
