d9e5052101b39dbc1445fc73c80fb4ecbda857b3
high1 <high1@mailfence.com>
2022-11-08 00:04:49 +0100

    build: configuration update
    - to be able to deploy the project both on GitHub and Netlify, base
    config property in vite is read from the environment
    - updated GitHub action script and Vite config to use the variables
    - renamed configuration files for ESLint and Prettier to their .json
    counterparts
    - added eslint-plugin-jsonc and enabled linting for json files, hidden
    files now linted by default

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv, type ConfigEnv } from 'vite';
 import preact from '@preact/preset-vite';
 import uno from 'unocss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  base: '/preact-clock/',
-  plugins: [preact(), uno(), tsconfigPaths()],
-});
+export default ({ mode }: ConfigEnv) =>
+  defineConfig({
+    base: loadEnv(mode, process.cwd(), '')['BASE'],
+    plugins: [preact(), uno(), tsconfigPaths()],
+  });
