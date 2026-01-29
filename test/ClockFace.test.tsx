f1d94ba117944a1514f49f35ae7279bdd54b324c
high1 <high1@users.noreply.github.com>
2026-01-30 00:40:09 +0100

    Feat/vitest browser preact (#133)
    * feat(test): Added tests
    
    * fix(test): Added playwright to workflow
    
    * fix(test): Added playwright to deps

---
+++ test/ClockFace.test.tsx
@@ -0,0 +1,17 @@
+import { describe, expect, test } from 'vitest';
+import { render } from 'vitest-browser-preact';
+
+import { ClockFace } from '@/ClockFace';
+import { clockFaceId } from '@/common';
+
+describe('<ClockFace />', () => {
+  test('renders clock face', () => {
+    const screen = render(<ClockFace />);
+    expect(screen.getByTestId(clockFaceId)).toBeInTheDocument();
+  });
+  test('unmounts clock face', () => {
+    const screen = render(<ClockFace />);
+    screen.unmount();
+    expect(screen.getByTestId(clockFaceId)).not.toBeInTheDocument();
+  });
+});
